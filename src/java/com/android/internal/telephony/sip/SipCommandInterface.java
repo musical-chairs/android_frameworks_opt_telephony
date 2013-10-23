e39a02a7251e17417c9048fbde907f59fff4daf1
jakew02 <jacob.whatley91@gmail.com>
2013-10-23 13:13:09 -0400

    fw_opt_telephony: Fix RIL for all Samsung devices
    By SferaDev and jakew02
    
    Add Samsung RIL files from CM
    
    Fix dependencies and errors at RIL.java
    
    Create Operators.java
    
    RIL: Add compatibility with qcom's DSDS:
    Devices with radio libraries built against a DSDS-enabled qualcomm
    BSP (anything in the last 4 or 5 months, apparently) have a slight
    incompatibility in the RIL protocol: it expects the first 4 bytes
    of the datastream to be the slot's identification (even if the
    device isn't dual-SIM). Absence of those 4 bytes will make the
    connection hang.
    
    Change-Id: Ie5894e4cf639b57b0e44bf63d6abb414448e9da0

--- src/java/com/android/internal/telephony/sip/SipCommandInterface.java
+++ src/java/com/android/internal/telephony/sip/SipCommandInterface.java
@@ -543,6 +543,8 @@ class SipCommandInterface extends BaseCommands implements CommandsInterface {
     public void getVoiceRadioTechnology(Message result) {
     }
 
+    public boolean needsOldRilFeature(String feature) { return false; }
+
     @Override
     public void getCellInfoList(Message result) {
     }
